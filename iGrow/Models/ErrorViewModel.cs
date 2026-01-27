1ea25cb008b3070e4b27d1531b2240e2df8b3e5a
dstoteva <infdtoteva@gmail.com>
2026-01-27 15:18:33 +0200

    Initial commit

---
+++ iGrow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iGrow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
